d579f6e7d540ce975158d6a4ca743515095a90ef
maxleclerc2 <mleclerc@et.esiea.fr>
2021-12-02 15:35:38 +0100

    Added test class list

--- lernejo-tester/src/test/java/fr/lernejo/tester/internal/TestClassDescriptionLernejoTests.java
+++ lernejo-tester/src/test/java/fr/lernejo/tester/internal/TestClassDescriptionLernejoTests.java
@@ -1,10 +1,12 @@
 package fr.lernejo.tester.internal;
 
 import fr.lernejo.tester.SomeLernejoTests;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,7 +18,16 @@ class TestClassDescriptionLernejoTests {
         TestClassDescription tester = new TestClassDescription(classToTest);
         List<Method> allMethods = tester.listTestMethods();
 
-        System.out.println(Arrays.toString(allMethods.toArray()));
+        List<String> expectedLines = new LinkedList<>();
+        expectedLines.add("ok");
+        expectedLines.add("ko");
+
+        List<String> resultLines = new LinkedList<>();
+        for (Method m: allMethods) {
+            resultLines.add(m.getName());
+        }
+
+        Assertions.assertLinesMatch(expectedLines, resultLines);
     }
 
     public static void main(String[] args) {
